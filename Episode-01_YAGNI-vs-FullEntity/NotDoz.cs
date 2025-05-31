1fa25d7a38b7acddb19defaf8c71965b1dd0e2d5
DozAndNotDoz <contact.dozbeyond@gmail.com>
2025-05-31 23:16:28 +0400

    Create NotDoz.cs

---
+++ Episode-01_YAGNI-vs-FullEntity/NotDoz.cs
@@ -0,0 +1,23 @@
+public class LicenseEntity
+{
+    public int Id { get; set; }
+    public string ApplicationNo { get; set; }
+    public string OwnerName { get; set; }
+    public string Status { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public bool IsArchived { get; set; }
+    public string LastModifiedBy { get; set; }
+    public string InternalComment { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string CompanyName { get; set; }
+    public string LicenseNumber { get; set; }
+    public DateTime? ExpiryDate { get; set; }
+    public bool IsForeign { get; set; }
+    public string Extra1 { get; set; }
+    public string Extra2 { get; set; }
+    public string Extra3 { get; set; }
+    public string Extra4 { get; set; }
+    public string Extra5 { get; set; }
+    public string Extra6 { get; set; }
+}
